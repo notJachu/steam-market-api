442f62ad9f8bff8b9a88e119d3255c10f14ca9e9
notJachu <jasiustasiu50@interia.pl>
2023-04-23 22:04:04 +0200

    fixed url, creating url, fetching url

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from functions.fetchSteam import test
+import functions.fetchSteam as steam
 
+
+data = {
+    "wear": "fn",
+    "collection": "danger_zone",
+    "quality": "normal",
+    "category": "mil-spec"
+}
 
 if __name__ == '__main__':
-    #test()
+    url = steam.make_url(data)
+    print(url)
+    res = steam.get_data(url)
+    print(res)
 
