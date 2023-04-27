bc58e4744219d9cc2dfcc66ef8a605a674b918b3
notJachu <jasiustasiu50@interia.pl>
2023-04-27 23:55:47 +0200

    ratio of prices

--- functions/tradeup.py
+++ functions/tradeup.py
@@ -9,3 +9,23 @@ def make_tradeup(options, allow_filler=False):
     options["category"] = "restricted"
     url = steam.make_url(options)
     purple = steam.get_data(url)
+
+
+def ratio(c1, c2, opt):
+    opt["category"] = c1
+    url = steam.make_url(opt)
+    r1 = steam.get_data(url)
+    opt["category"] = c2
+    url = steam.make_url(opt)
+    r2 = steam.get_data(url)
+    a1 = 0
+    a2 = 0
+    for i in r1:
+        a1 += i[1]
+    a1 /= len(r1)
+
+    for i in r2:
+        a2 += i[1]
+    a2 /= len(r2)
+
+    return a2/a1
